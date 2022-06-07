5ef74771b69eb7e76c05a6444c7cb26785ea0a45
huseyin <huseyinarslan06@gmail.com>
2022-06-07 11:28:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const AC = document.querySelector(".row1-1");
+const plusMinus = document.querySelector(".row1-2");
+const modulus = document.querySelector(".row1-3");
+const division = document.querySelector(".row1-4");
+const seven = document.querySelector(".row2-1");
+const eight = document.querySelector(".row2-2");
+const nine = document.querySelector(".row2-3");
+const multiplication = document.querySelector(".row2-4");
+const four = document.querySelector(".row3-1");
+const five = document.querySelector(".row3-2");
+const six = document.querySelector(".row3-3");
+const minus = document.querySelector(".row3-4");
+const one = document.querySelector(".row4-1");
+const two = document.querySelector(".row4-2");
+const three = document.querySelector(".row4-3");
+const plus = document.querySelector(".row4-4");
+const zero = document.querySelector(".row5-1");
+const dot = document.querySelector(".row5-2");
+const equal = document.querySelector(".row5-3");
+
+AC.addEventListener("click", () => {
+    document.getElementById("result").innerText = "";
+});
+
+plusMinus.addEventListener("click", () => {
+    document.getElementById("operation").innerText += "-";
+});
+
+modulus.addEventListener("click", () => {
+    document.getElementById("operation").innerText += "%"
+});
+
+division.addEventListener("click", () => {
+    document.getElementById("operation").innerText += "÷"
+});
+
+seven.addEventListener("click", () => {
+    document.getElementById("operation").innerText += "7"
+});
+
+eight.addEventListener("click", () => {
+    document.getElementById("operation").innerHTML += "8"
+});
+
+nine.addEventListener("click", () => {
+    document.getElementById("operation").innerText += "9"
+});
+
+
